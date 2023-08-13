49f0d0e8d443d51ba564e7d28b1068b110330176
Kizzzy <xuhua0174@163.com>
2023-08-14 00:30:57 +0800

    [qqt] * show border in ImgDisplay

--- src/main/java/cn/kizzzy/qqt/display/ImgDisplay.java
+++ src/main/java/cn/kizzzy/qqt/display/ImgDisplay.java
@@ -2,9 +2,9 @@ package cn.kizzzy.qqt.display;
 
 import cn.kizzzy.javafx.display.DisplayLoaderAttribute;
 import cn.kizzzy.javafx.display.image.Frame;
-import cn.kizzzy.javafx.display.image.Track;
 import cn.kizzzy.javafx.display.image.ImageArg;
 import cn.kizzzy.javafx.display.image.ImageDisplayLoader;
+import cn.kizzzy.javafx.display.image.Track;
 import cn.kizzzy.qqt.ImgFile;
 import cn.kizzzy.qqt.helper.QqtImgHelper;
 import cn.kizzzy.vfs.IPackage;
@@ -26,27 +26,31 @@ public class ImgDisplay implements ImageDisplayLoader {
         
         Track track = new Track();
         int i = 0;
-        for (ImgFile.Frame item : img.frames) {
-            BufferedImage image = QqtImgHelper.toImage(item);
+        for (ImgFile.Frame frame : img.frames) {
+            BufferedImage image = QqtImgHelper.toImage(frame);
             if (image != null) {
-                float offsetX = -img.maxWidth / 2f - img.offsetX + item.offsetX;
-                float offsetY = -img.maxHeight - img.offsetY + item.offsetY + 20;
+                float offsetX = -img.maxWidth / 2f - img.offsetX + frame.offsetX;
+                float offsetY = -img.maxHeight - img.offsetY + frame.offsetY + 20;
                 
-                Frame frame = new Frame();
-                frame.x = offsetX;
-                frame.y = offsetY;
-                frame.width = item.width;
-                frame.height = item.height;
-                frame.image = image;
-                frame.time = 167;
-                frame.extra = String.format("%02d/%02d", i + 1, img.count);
+                Frame f = new Frame();
+                f.x = offsetX;
+                f.y = offsetY;
+                f.width = frame.width;
+                f.height = frame.height;
+                f.image = image;
+                f.time = 167;
+                f.extra = String.format("%02d/%02d", i + 1, img.count);
                 
-                track.frames.add(frame);
+                track.frames.add(f);
             }
             i++;
         }
         
         ImageArg arg = new ImageArg();
+        arg.borderX = arg.pivotX - img.maxWidth / 2f;
+        arg.borderY = arg.pivotY - 100;
+        arg.borderW = 100;
+        arg.borderH = 100;
         arg.tracks.add(track);
         return arg;
     }
